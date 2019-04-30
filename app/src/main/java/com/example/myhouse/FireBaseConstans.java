2ceb7133b42c40b63e4d12e24594651ab0251883
thinhbkka <thinhbk.ka@gmail.com>
2019-04-30 11:31:01 +0700

    .xyz

--- app/src/main/java/com/example/myhouse/FireBaseConstans.java
+++
@@ -1,8 +0,0 @@
-package com.example.myhouse;
-
-public class FireBaseConstans {
-    public static final String ROOM = "room";
-    public static final String LIVINGROOM = "livingroom";
-    public static final String BEDROOM = "bedroom";
-    public static final String KITCHEN = "kitchen";
-}
